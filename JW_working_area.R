9c9f6fc20c4434d163dee96b9cf2c02c46defd8b
Jonathan Wray <Jonathan.Wray@nice.org.uk>
2022-11-24 08:53:15 +0000

    Adding charts to cookbook

--- JW_working_area.R
+++ JW_working_area.R
@@ -8,53 +8,19 @@ doacs_df <- readr::read_csv("data/DOACs_data.csv")
 
 # Histogram ---------------------------------------------------------------
 
-# No theme
-histogram <- iris %>%
-  ggplot(aes(x = Sepal.Width)) +
-  geom_histogram(aes(fill = Species), binwidth = 0.2, color = "black") +
-  geom_hline(yintercept = 0, linewidth = 1, colour = "#333333") +
-  scale_y_continuous(expand = c(0,0), limits = c(0,40)) +
-  labs(title = "Species setosa has the longest sepal length",
-       subtitle = "Distribution of sepal width by species",
-       x = "Sepal Width",
-       y = "Frequency")
+### Options from ONS cookbook ###
 
-histogram
-
-# NICE theme
-nice_hist <- histogram +
-  nice_gg_theme(legend = "right", xlabel = TRUE) +
-  labs(title = "Species setosa has the longest sepal length",
-       subtitle = "Distribution of sepal width by species",
-       x = "Sepal Width",
-       y = "Frequency")
-
-nice_hist
-
-nice_hist <- finalise_plot(nice_hist,
-              source_name = "Source: Iris dataset",
-              logo_image_path = "data/NICE_short_logo_black.png")
-
-nice_hist
+## Done
+#- maintained a consistent font
+#- gridlines should be light gra
 
 
-# Working -----------------------------------------------------------------
 
-nice_hist <- iris %>%
-  ggplot(aes(x = Sepal.Width)) +
-  geom_histogram(aes(fill = Species), binwidth = 0.2, color = "black") +
-  geom_hline(yintercept = 0, linewidth = 1, colour = "#333333") +
-  scale_y_continuous(expand = c(0,0), limits = c(0,40)) +
-  nice_gg_simple() +
-  theme(legend.position = "none") +
-  labs(title = "Species setosa has the longest sepal length",
-       subtitle = "Distribution of sepal width by species",
-       x = "Sepal Width",
-       y = "Frequency")
 
-nice_hist
-
-finalise_plot(nice_hist,
-              source_name = "Source: Iris dataset",
-              logo = )
-
+## Not done
+#- Keep axis font same size as surrounding text
+#- Generally between 4 and 8 grid lines per chart
+#-
+#-
+#-
+#-
