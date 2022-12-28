3dc98f7bdfcc7ea7d129f4921cba6d41e8c0a258
Vanessa <vkwacko007@gmail.com>
2022-12-28 17:31:17 +0000

    Function documentation for nice_plotly_theme()

--- R/plotly_theme.R
+++ R/plotly_theme.R
@@ -1,29 +1,53 @@
-# Setup
-if (!require(pacman)) install.packages("pacman")
-pacman::p_load(tidyverse, plotly)
-
-here::i_am("R/plotly_theme.R")
-
-pacman::p_load(here)
-
-# Load NICE colours
-source(here("R", "load_nice_colours.R"))
-
-# Criteria
-# - Font: Inter Regular
-# - White background (already a default) and no border
-# - Axis labels font size 12, black. Margins
-# - Grid lines - horizontal only if vertical bar chart, horizontal only if line chart, vertical only if horizontal bar chart, both if scatter
-# - Tool bar icons - download
-# - y axis label position
-# - Plot margins
-
-# - Black line around bars
-# Title and subtitle and caption and data source as text below graph
-
+#' Apply NICE Plotly theme
+#'
+#' @description
+#' `nice_plotly_theme()` applies a NICE theme to a Plotly chart object. It makes
+#' some basic changes, including:
+#' - Changing the font to Inter
+#' - Setting the minimum font size
+#' - Adding or removing grid lines depending on the chart type
+#' - Reminds users to add x and y axis titles
+#' - Removes icons from the default Plotly tool bar
+#'
+#' @param p Plotly chart object.
+#' @param chart_type Type of chart, determines which grid lines are shown:
+#' - `"vertical_bar"` (default): For bar charts and histograms. Only shows
+#' horizontal grid lines.
+#' - `"horizontal_bar"`: For horizontal bar charts. Only shows vertical grid
+#' lines.
+#' - `"scatter"`: For scatter plots. Shows both horizontal and vertical grid
+#' lines.
+#' - `"line"`: For line charts. Only shows horizontal grid lines.
+#' @param x_title,y_title String to use as X or y axis title.
+#' @param font_size Minimum font size. Default is `12`.
+#' @param pad_axes Add space between the axis lines and the axis title or tick
+#'    marks? If `TRUE` (default), adds some space between the tick labels and
+#'    the axis lines. If using tick marks, set to `FALSE` to avoid a gap between
+#'    the axis lines and the tick marks.
+#'
+#' @returns A Plotly chart object.
+#'
+#' @examples
+#'# Load data
+#' iris_bar_df <- iris %>%
+#'    group_by(Species) %>%
+#'    summarise(Sepal_Width = mean(Sepal.Width)) %>%
+#'    ungroup()
+#'
+#' # Create formatted chart
+#' plot_ly(iris_bar_df,
+#'    x = ~Species,
+#'    y = ~Sepal_Width,
+#'    type = "bar",
+#'    marker = list(color = nice_colours_full[["bold_teal_100"]],
+#'                  line = list(color = nice_colours_full[['black_100']], width = 1.5))) %>%
+#'    # Apply NICE theme
+#'    nice_plotly_theme(chart_type = "vertical_bar",
+#'                      x_title = "Species",
+#'                      y_title = "Sepal Width")
 
 nice_plotly_theme <- function(p,
-                       chart_type = "bar",
+                       chart_type = "vertical_bar",
                        x_title = "[x axis title]",
                        y_title = "[y axis title]",
                        font_size = 12,
