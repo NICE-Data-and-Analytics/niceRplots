f940c56ddaa43d04a8f05c16275fac14d3273233
Jonathan Wray <Jonathan.Wray@nice.org.uk>
2023-02-02 16:17:49 +0000

    alter nice_gg_theme() to add optional arguments

--- R/nice_gg_theme.R
+++ R/nice_gg_theme.R
@@ -1,13 +1,37 @@
-#' Add the NICE theme to a chart
+#' Apply the NICE theme to a chart
 #'
-#'This function will style your ggplot2 charts with the NICE theme. This will
-#'adjust the fonts, text hierarchy, background, margins and colour scheme.
+#'@description
 #'
-#' @param base_size The base font size of your plot. Changing this will alter text
-#' sizes throughout the plot. Defaults to size 12.
+#' `nice_gg_theme()`applies a nice theme to a ggplot2 chart object to make it
+#' consistent with the NICE style. It makes some basic changes, including:
+#' - Changing the chart title font to Lora SemiBold
+#' - changing the base font to Inter Regular
+#' - Applies a hierarchy for text size
+#' - Adjusts the chart background and margins
+#' - Adjusts the chart colour scheme
+#' - Provides an option to remove axis titles
+#' - Provides an option to alter grid lines
+#' - Provides an option to add a panel border
+#' - Provides an option to remove the legend
+#'
+#' @param base_size Minimum font size. Default is `12`.
+#' @param x_title Option to remove the x axis title. If `TRUE`(default), the x axis
+#' title will be present and can be renamed using the `labs()` function. If set to
+#' `FALSE`, the x axis title will be removed.
+#' @param y_title Option to remove the y axis title. If `TRUE`(default), the y axis
+#' title will be present and can be renamed using the `labs()` function. If set to
+#' `FALSE`, the y axis title will be removed.
+#' @param panel_border Option to add a panel border. If `FALSE`(default), no border
+#' will be present. If set to `TRUE`, a panel border will be added.
+#' @param grid_lines determines which major grid lines are shown:
+#' -`"y"` (default): show y axis grid lines
+#' -`"x"`: show x axis grid lines
+#' -`"x+y"`: show both x and y axis grid lines
+#' -`"none"`: remove grid lines
+#' @param legend Option to remove the chart legend. If `TRUE`(default), the legend
+#' will be present above the chart. If set to `FALSE`, the legend will be removed.
 #'
 #' @return A NICE styled ggplot2 object
-#'
 #' @export
 #'
 #' @importFrom dplyr %>%
@@ -25,9 +49,21 @@
 #' geom_col(aes(x = Species, y = Sepal.Width), fill = "#228096", colour = "#000000") +
 #' geom_hline(yintercept = 0, linewidth = 1, colour = "#333333") +
 #' scale_y_continuous(expand = c(0, 0), limits = c(0, 4)) +
-#' nice_gg_theme(base_size = 12)
+#' nice_gg_theme(
+#'   base_size = 12,
+#'   x_title = FALSE,
+#'   y_title = TRUE,
+#'   panel_border = FALSE,
+#'   grid_lines = "y",
+#'   legend = TRUE
+#' )
 
-nice_gg_theme <- function(base_size = 12){
+nice_gg_theme <- function(base_size = 12,
+                          x_title = TRUE,
+                          y_title = TRUE,
+                          panel_border = FALSE,
+                          grid_lines = "y",
+                          legend = TRUE){
 
   font <- "Inter Regular"
   heading_font <- "Lora SemiBold"
@@ -79,10 +115,6 @@ nice_gg_theme <- function(base_size = 12){
 
     # Format the grid lines ---------------------------------------------------
 
-    # Adjust major grid lines
-    panel.grid.major.x = ggplot2::element_blank(),
-    panel.grid.major.y = ggplot2::element_line(color = "#BFBFBF"),
-
     # Adjust minor grid lines
     panel.grid.minor = ggplot2::element_blank(),
 
@@ -119,8 +151,83 @@ nice_gg_theme <- function(base_size = 12){
 
     # Add spacing around the plot
     plot.margin = ggplot2::margin(0.25, 0.25, 0.25, 0.25,"cm")
-
   )
+
+
+# Optional adjustments ----------------------------------------------------
+
+  # Option to remove x axis title
+  if (!x_title){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        axis.title.x = ggplot2::element_blank())
+  }
+
+
+  # Option to remove y axis title
+  if (!y_title){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        axis.title.y = ggplot2::element_blank())
+  }
+
+
+  # Option to add a panel border (useful for facet plots)
+  if (panel_border){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        panel.border = ggplot2::element_rect(color = "#000000",
+                                             linewidth = 0.3,
+                                             fill = NA))
+  }
+
+  # Option to alter grid lines
+  if (grid_lines == "y"){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        panel.grid.major.y = ggplot2::element_line(color = "#BFBFBF"),
+        panel.grid.major.x = ggplot2::element_blank()
+      )
+
+  } else if(grid_lines == "x"){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        panel.grid.major.y = ggplot2::element_blank(),
+        panel.grid.major.x = ggplot2::element_line(color = "#BFBFBF"))
+
+
+  } else if(grid_lines == "x+y"){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        panel.grid.major.y = ggplot2::element_line(color = "#BFBFBF"),
+        panel.grid.major.x = ggplot2::element_line(color = "#BFBFBF")
+      )
+
+  } else if(grid_lines == "none"){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        panel.grid.major.y = ggplot2::element_blank(),
+        panel.grid.major.x = ggplot2::element_blank()
+      )
+  }
+
+
+  # Option to remove legend axis title
+  if (!legend){
+
+    nice_theme <- nice_theme +
+      ggplot2::theme(
+        legend.position = "none")
+  }
 
   return(nice_theme)
 }
+
+
